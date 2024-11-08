f0c9cc9368844fec340e83af16f2c929bf7a8fec
Adam Maas <adamjm1220@gmail.com>
2024-11-08 09:46:13 -0500

    Lambda_Test

--- src/main/java/com/example/lambdatemplate/api/Handler/LambdaHandler.java
+++ src/main/java/com/example/lambdatemplate/api/Handler/LambdaHandler.java
@@ -14,7 +14,6 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.example.lambdatemplate.config.LambdaConfig;
 import com.example.lambdatemplate.service.CheckStatus;
-// import org.springframework.core.env.PropertySource;
 
 public class LambdaHandler implements RequestHandler<Object, Object> {
     private CheckStatus checkStatus;
